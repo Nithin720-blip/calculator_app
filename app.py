f4020ac4779b46c90c7eeb72460ece1008abf1a6
Nithin720-blip <nithinsn26@gmail.com>
2026-05-22 12:42:07 +0530

    Fix Azure deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -9,28 +10,35 @@ def index():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     data = request.get_json()
+
     num1 = float(data.get('num1', 0))
     num2 = float(data.get('num2', 0))
     operator = data.get('operator', '+')
-    
-    result = 0
+
     try:
         if operator == '+':
             result = num1 + num2
+
         elif operator == '-':
             result = num1 - num2
+
         elif operator == '*':
             result = num1 * num2
+
         elif operator == '/':
             if num2 == 0:
                 return jsonify({'error': 'Cannot divide by zero'}), 400
             result = num1 / num2
+
         else:
             return jsonify({'error': 'Invalid operator'}), 400
+
+        return jsonify({'result': result})
+
     except Exception as e:
-         return jsonify({'error': str(e)}), 400
-         
-    return jsonify({'result': result})
+        return jsonify({'error': str(e)}), 400
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
