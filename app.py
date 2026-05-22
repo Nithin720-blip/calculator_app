4e979e1ce74f1fed18d4bfdbfb4403cb8916c7ba
Nithin720-blip <nithinsn26@gmail.com>
2026-05-22 18:46:44 +0530

    hi

--- app.py
+++ app.py
@@ -39,13 +39,6 @@ def calculate():
         return jsonify({'error': str(e)}), 400
 
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
-import os
-
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 80))
-    )
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
